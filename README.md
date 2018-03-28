3ffc4d54bd9424eeab4b79e7102eee18fb3c3c96
fm <fabian@mikolajczak.org>
2018-03-28 12:19:29 +0200

    Project setup

---
+++ README.md
@@ -0,0 +1,11 @@
+# Popular Movie for Android
+
+Project for Android Developer Nanodegree 
+
+# themoviedb.com API
+
+Please complete value of "themoviedb_api_key" in strings.xml
+with API key to themoviedb.com service.
+
+
+
--- app/src/main/java/org/mikolajczak/popularmovies/DetailActivity.java
+++ app/src/main/java/org/mikolajczak/popularmovies/DetailActivity.java
@@ -2,14 +2,12 @@ package org.mikolajczak.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
-public class DiscoveryActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_discovery);
-
+        setContentView(R.layout.activity_detail);
     }
 }
